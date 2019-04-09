462f4c1278cb2bccff01c162ce3d8153d90adbd3
HiXY <fear1396@mail.ru>
2019-04-09 17:23:38 +0300

    Refactoring of base class and added 2 methods.

--- Second_Task/num_diff.py
+++ Second_Task/num_diff.py
@@ -1,4 +1,4 @@
-class DerivativeRightDiff:
+class Derivative:
     def __init__ (self, f = 0, h = 0):
         self.__f = f
         self.__h = h
@@ -9,69 +9,56 @@ class DerivativeRightDiff:
     def set_step (self, h):
         self.__h = h
 
+    def get_function (self):
+        return self.__f
+
+    def get_step (self):
+        return self.__h
+
+
+class DerivativeRightDiff (Derivative):
+    def __init__ (self, f = 0, h = 0):
+        Derivative.__init__ (self, f, h)
+
     def __call__ (self, x):
-        f, h = self.__f, self.__h
+        f, h = self.get_function (), self.get_step ()
         return ((f (x + h) - f (x)) / h)
 
 
-class DerivativeLeftDiff (DerivativeRightDiff):
+class DerivativeLeftDiff (Derivative):
     def __init__ (self, f = 0, h = 0):
-       DerivativeRightDiff.__init__ (self, f, h)
-
-    def set_function (self, f):
-        self.__f = f
-
-    def set_step (self, h):
-        self.__h = h
+       Derivative.__init__ (self, f, h)
 
     def __call__ (self, x):
-        f, h = self.__f, self.__h
+        f, h = self.get_function (), self.get_step ()
         return ((f (x) - f (x - h)) / h)
 
 
-class DerivativeCentrDiff (DerivativeLeftDiff):
+class DerivativeCentrDiff (Derivative):
     def __init__ (self, f = 0, h = 0):
-        DerivativeLeftDiff.__init__ (self, f, h)
-
-    def set_function (self, f):
-        self.__f = f
-
-    def set_step (self, h):
-        self.__h = h
+        Derivative.__init__ (self, f, h)
 
     def __call__ (self, x):
-        f, h = self.__f, self.__h
+        f, h = self.get_function (),self.get_step ()
         return ((f (x + h) - f (x - h)) / (2. * h))
 
 
-class DerivativeSecAccDiff (DerivativeCentrDiff):
+class DerivativeSecAccDiff (Derivative):
     def __init__ (self, f = 0, h = 0):
-        DerivativeCentrDiff.__init__ (self, f, h)
-
-    def set_function (self, f):
-        self.__f = f
-
-    def set_step (self, h):
-        self.__h = h
+        Derivative.__init__ (self, f, h)
 
     def __call__ (self, x):
-        f, h = self.__f, self.__h
+        f, h = self.get_function (), self.get_step ()
         return (((4./ (3. * 2. * h)) * (f (x + h) -  f (x - h))) - \
                 ((1. / (3. * 4. * h)) * (f (x + (2. * h)) - f (x - (2. * h)))))
 
 
-class DerivativeThirdAccDiff (DerivativeSecAccDiff):
+class DerivativeThirdAccDiff (Derivative):
     def __init__ (self, f = 0, h = 0):
-        DerivativeSecAccDiff.__init__ (self, f, h)
-
-    def set_function (self, f):
-        self.__f = f
-
-    def set_step (self, h):
-        self.__h = h
+        Derivative.__init__ (self, f, h)
 
     def __call__ (self, x):
-        f, h = self.__f, self.__h
+        f, h = self.get_function (), self.get_step ()
         return (((3. / (2. * 2. * h)) * (f (x + h) - f (x - h))) - \
                 ((3. / (5. * 4. * h)) * (f (x + (2. * h)) - f (x - (2. * h)))) + \
                 ((1. / (10. * 6. * h)) * (f (x + (3. * h)) - f (x - (3. * h)))))
